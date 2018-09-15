b6a7e2fc97b4dde08533053f5e805a982e5fdc0d
DragonflyStats <kobriendublin@gmail.com>
2018-09-15 11:39:46 +0100

    Create Measles.R

---
+++ Measles.R
@@ -0,0 +1,16 @@
+# Measles
+
+#### 1. Load R packages ####
+library(dplyr)
+library(magrittr)
+
+#### 2. Load and Clean Measles Data ####
+Measles <- read_csv("https://raw.githubusercontent.com/RWorkshop/AfricaDataDive/master/Nigeria_Southern_State_Measles_Outreach_Data.csv")
+
+#### 3. Load a Nigerian Administrative Map Shapefile ####
+
+
+#### 4. Inspect the Cleaned Data ####
+
+head(Measles)
+
